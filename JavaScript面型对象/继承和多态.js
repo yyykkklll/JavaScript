943ee5e3f47f0f08a447d7648a06417ffc7f1cde
kelong <1175265528@qq.com>
2025-03-21 11:26:16 +0800

    继承和多态

---
+++ JavaScript面型对象/继承和多态.js
@@ -0,0 +1,23 @@
+// ES6 class实现继承
+class animial {
+  constructor(name) {
+    this.name = name;
+  }
+  eat() {
+    console.log(this.name + "吃东西");
+  }
+}
+
+class cat extends animial {
+  constructor(nane, color) {
+    super(nane);
+    this.color = color;
+  }
+  meow() {
+    console.log(this.name + "喵喵叫");
+  }
+}
+
+cat1 = new cat("小猫", "白色");
+cat1.eat();
+cat1.meow();
