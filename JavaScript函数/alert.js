85f60dbdae87d32fb03a60f347a8007183ae6c8a
kelong <1175265528@qq.com>
2025-07-24 09:40:41 +0800

    add test file

--- JavaScript函数/alert.js
+++ JavaScript函数/alert.js
@@ -1,2 +1,14 @@
-alert("This is an alert message!");
-console.log("Alert has been triggered.");
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([5, 3, 8, 4, 2])); // [2, 3, 4, 5, 8]
